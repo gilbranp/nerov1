7726d48b15470d57d0b4d91ba14ba60f77022ae6
Yanzz <98795764+Yanzz-Bot@users.noreply.github.com>
2022-04-16 14:24:48 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
-_*SC? YT BANYAK🗿*_
+_*SC : https://github.com/Yanzz-Bot/Yanz-V1*_
 `.trim(), '©Ynz🥀', 'oke Bang', 'Thanks')
 handler.help = ['sourcecode']
 handler.tags = ['info']
