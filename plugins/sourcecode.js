474db27d5adf4620c6559be3766e9a029a1cc05d
Yanzz <98795764+Yanzz-Bot@users.noreply.github.com>
2022-04-18 05:20:32 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -3,7 +3,8 @@ let fetch = require('node-fetch')
 let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Source+code'
 let info = `╭═[ *SCRIPT* ]═────···
 ┴
-│ *Script :* PRIVATE!
+│ *Script :* 
+│ ╰ https://github.com/Yanzz-Bot/Yanz-v1
 │ *Base :*
 │ ╰ https://github.com/nurutomo/wabot-aq
 │ *RestApi :* 
