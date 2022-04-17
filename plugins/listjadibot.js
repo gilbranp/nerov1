b5833de496fc2117959706e071b386dba0c9c792
Yanzz <98795764+Yanzz-Bot@users.noreply.github.com>
2022-04-18 05:25:37 +0700

    Update listjadibot.js

--- plugins/listjadibot.js
+++ plugins/listjadibot.js
@@ -1,6 +1,6 @@
 async function handler(m, { usedPrefix }) {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
-  m.reply( `🌟 *Bot Utama :*\n⤷ wa.me/6282186172016?text=.menu (Yanz)\n\n`+ `🤖 *JadiBot :*\n` + users.map(v => '✎ wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
+  m.reply( `🤴 *Owner Yanz Bot* : *\n⤷ wa.me/6288272489919?text=Assalamualaikum+Kak (Owner)\n\n`+ `🌟 *Bot Utama :*\n⤷ wa.me/6282186172016?text=.menu (Yanz)\n\n`+ `🤖 *JadiBot :*\n` + users.map(v => '✎ wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
 }
 handler.command = handler.help = ['listjadibot','listbot','ljb']
 handler.tags = ['jadibot']
