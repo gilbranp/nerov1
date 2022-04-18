3909c6e8f620e4e9f2704ce367028248b37e3e5e
Yanzz <98795764+Yanzz-Bot@users.noreply.github.com>
2022-04-18 16:04:18 +0700

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, text }) => {
+  let ext= `
+BOT ON..
+`.trim()
+conn.send2ButtonLoc(m.chat, await (await fetch(flu + '𝚈𝚊𝚗𝚣 𝙱𝚘𝚝')).buffer(), ext, '𝚈𝚊𝚗𝚣 𝙱𝚘𝚝 𝚂𝚒𝚊𝚙 𝙼𝚎𝚖𝚋𝚊𝚗𝚝𝚞 𝙺𝚊𝚖𝚞 (*´ω｀*)', 'Menu', '.?', 'Owner', '.nowner', m)
+
+}
+handler.customPrefix = /^(tes|tess|test)$/i
+handler.command = new RegExp
+
+module.exports = handler
+let wm = global.botwm
