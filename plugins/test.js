3f44768b88c603827949c675cf08f21a1dacf41a
Yanzz <98795764+Yanzz-Bot@users.noreply.github.com>
2022-04-19 10:30:34 +0700

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,7 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
   let ext= `
-BOT ON..
+Status: On...
+Silahkan Ketik .menu Untuk Memulai Bot...
 `.trim()
 conn.send2ButtonLoc(m.chat, await (await fetch(flu + '𝚈𝚊𝚗𝚣 𝙱𝚘𝚝')).buffer(), ext, '𝚈𝚊𝚗𝚣 𝙱𝚘𝚝 𝚂𝚒𝚊𝚙 𝙼𝚎𝚖𝚋𝚊𝚗𝚝𝚞 𝙺𝚊𝚖𝚞 (*´ω｀*)', 'Menu', '.?', 'Owner', '.nowner', m)
 
