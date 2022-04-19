1cc144498fdca2ec0cc2c53730139d884c384731
Yanzz <98795764+Yanzz-Bot@users.noreply.github.com>
2022-04-19 17:16:42 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,23 +1,39 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let fetch = require('node-fetch')
+	let fetch = require('node-fetch')
+    let fs = require('fs')
+const ftrol = {
+    key : {
+    remoteJid: 'status@broadcast',
+    participant : '0@s.whatsapp.net'
+    },
+    message: {
+    orderMessage: {
+    itemCount : 9999,
+    status: 1,
+    surface : 1,
+    message: 'Sc Yanz Bot🗿', 
+    orderTitle: `▮Menu ▸`,
+    thumbnail: await (await fetch(fla + 'Script')).buffer(), //Gambarnye
+    sellerJid: '0@s.whatsapp.net' 
+    }
+    }
+    }
 let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Source+code'
-let info = `╭═[ *SCRIPT* ]═────···
-┴
-│ *Script :* 
-│ ╰ https://github.com/Yanzz-Bot/Yanzz-v1
-│ *Base :*
-│ ╰ https://github.com/nurutomo/wabot-aq
-│ *RestApi :* 
-│ ╰ https://hardianto.xyz
-┬
-╰──────────═┅═──────────
-[❗] Jangan diperjual belikan tanpa seizin owner
+let info = `◪ 📮 *SCRIPT BOT*
+│ *Via Mediafree:*
+│ ╰ *Gda*
+│
+│ *Via Github:*
+│ ╰* https://github.com/Yanzz-Bot/Yanzz-V1*
+╰─────═┅═────────
+📍 *N o t e :* 
+• Jangan lupa minta izin owner sebelum menggunakan scriptnya kak!
+• Jangan Dijual Ya Kak!
 `.trim()
-await conn.send2Button(m.chat, info, `${botol}`, '⋮☰ Back', '.menu', 'Owner', '.nowner', m)
-conn.reply(info)
+await conn.send2Button(m.chat, info, `🎮 Ynz`, 'Ok Ynz', 'Thanks', 'OWNER', '.owner', ftrol)
 }
 handler.help = ['sc']
-handler.tags = ['tutor']
+handler.tags = ['info']
 handler.command = /^(sc)$/i
 handler.owner = false
 handler.mods = false
